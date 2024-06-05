3781f34268691b3e960f5bc97ab4818297f84bbf
Faisal Sattari <fs422@ic.ac.uk>
2024-06-05 13:40:19 +0100

    feat: added button to show a hint in flashcard

--- lib/pages/flashcards.dart
+++ lib/pages/flashcards.dart
@@ -10,6 +10,7 @@ class Flashcard {
   final String definition;
   final List<String> synonyms;
   final String imageUrl;
+  final String hint;
 
   Flashcard({
     required this.word,
@@ -17,6 +18,7 @@ class Flashcard {
     required this.definition,
     required this.synonyms,
     required this.imageUrl,
+    required this.hint,
   });
 }
 
@@ -30,7 +32,8 @@ class WordsListPage extends StatelessWidget {
             'A greeting used to begin a conversation or to acknowledge someone\'s presence.',
         synonyms: ['صباح الخير', 'ترحيب'],
         imageUrl:
-            'https://static.independent.co.uk/s3fs-public/thumbnails/image/2017/04/03/17/donald-trump-sisi.jpg?width=1200'),
+            'https://static.independent.co.uk/s3fs-public/thumbnails/image/2017/04/03/17/donald-trump-sisi.jpg?width=1200',
+        hint: 'This word is used to greet someone.'),
     Flashcard(
         word: 'ترحيب',
         translation: 'welcome',
@@ -38,7 +41,8 @@ class WordsListPage extends StatelessWidget {
             'A greeting or expression of goodwill given upon someone\'s arrival.',
         synonyms: ['صباح الخير', 'مرحبًا'],
         imageUrl:
-            'https://previews.123rf.com/images/perig76/perig761410/perig76141000059/32314128-view-of-a-young-attractive-man-welcoming-you-in-his-house.jpg'),
+            'https://previews.123rf.com/images/perig76/perig761410/perig76141000059/32314128-view-of-a-young-attractive-man-welcoming-you-in-his-house.jpg',
+            hint: 'This word is used to greet someone.'),
     Flashcard(
         word: 'صباح الخير',
         translation: 'good morning',
@@ -46,7 +50,8 @@ class WordsListPage extends StatelessWidget {
             'A greeting used to acknowledge or welcome someone during the morning hours.',
         synonyms: ['ترحيب', 'مرحبًا'],
         imageUrl:
-            'https://i.pinimg.com/736x/d1/8a/69/d18a69b3b01b6f80aa192e26fc622324.jpg'),
+            'https://i.pinimg.com/736x/d1/8a/69/d18a69b3b01b6f80aa192e26fc622324.jpg',
+        hint: 'This word is used to greet someone in the morning.'),
   ];
 
   WordsListPage({super.key});
@@ -198,110 +203,125 @@ class SpinWordWidgetState extends State<SpinWordWidget>
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: _toggleWord,
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          AnimatedBuilder(
-            animation: _rotationAnimation,
-            builder: (context, child) {
-              return SlideTransition(
-                position: _slideUpAnimation,
-                child: FadeTransition(
-                  opacity: _fadeInFadeOutAnimation,
-                  child: Visibility(
-                    visible: _rotationAnimation.value > 0.5,
-                    child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Image.network(
-                            widget.flashcard.imageUrl,
-                            height: 150,
-                          ),
-                          const SizedBox(height: 20),
-                        ]),
-                  ),
-                ),
-              );
-            },
-          ),
-          AnimatedBuilder(
-            animation: _rotationAnimation,
-            builder: (context, child) {
-              return Transform(
-                alignment: FractionalOffset.center,
-                transform: Matrix4.rotationY(_rotationAnimation.value * 3.14),
-                child: _rotationAnimation.value <= 0.5
-                    ? Text(widget.flashcard.word,
-                        style: const TextStyle(fontSize: 28))
-                    : Transform(
-                        alignment: Alignment.center,
-                        transform: Matrix4.rotationY(3.14),
-                        child: Text(widget.flashcard.translation,
-                            style: const TextStyle(fontSize: 28)),
-                      ),
-              );
-            },
-          ),
-          AnimatedBuilder(
-            animation: _rotationAnimation,
-            builder: (context, child) {
-              return SlideTransition(
-                position: _slideDownAnimation,
-                child: FadeTransition(
-                  opacity: _fadeInFadeOutAnimation,
-                  child: Visibility(
-                    visible: _rotationAnimation.value > 0.5,
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        const SizedBox(height: 20),
-                        Text(
-                          widget.flashcard.definition,
-                          style: const TextStyle(
-                              fontSize: 20, fontStyle: FontStyle.italic),
-                          textAlign: TextAlign.center,
-                        ),
-                        const SizedBox(height: 10),
-                        Wrap(
-                          spacing: 8.0, // gap between adjacent chips
-                          runSpacing: 4.0, // gap between lines
-                          children:
-                              widget.flashcard.synonyms.map((String synonym) {
-                            return GestureDetector(
-                              onTap: () {
-                                Flashcard? flashcard = widget.flashcards
-                                    .firstWhere((card) => card.word == synonym,
-                                        orElse: () => null as Flashcard);
-                                if (flashcard != null) {
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (context) => FlashcardPage(
-                                        flashcard: flashcard,
-                                        flashcards: widget.flashcards,
-                                      ),
-                                    ),
-                                  );
-                                }
-                              },
-                              child: Chip(
-                                label: Text(synonym),
+    return Scaffold(
+      body: Center(
+        child: GestureDetector(
+          onTap: _toggleWord,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              AnimatedBuilder(
+                animation: _rotationAnimation,
+                builder: (context, child) {
+                  return SlideTransition(
+                    position: _slideUpAnimation,
+                    child: FadeTransition(
+                      opacity: _fadeInFadeOutAnimation,
+                      child: Visibility(
+                        visible: _rotationAnimation.value > 0.5,
+                        child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Image.network(
+                                widget.flashcard.imageUrl,
+                                height: 150,
                               ),
-                            );
-                          }).toList(),
-                        ),
-                      ],
+                              const SizedBox(height: 20),
+                            ]),
+                      ),
                     ),
-                  ),
-                ),
-              );
-            },
+                  );
+                },
+              ),
+              AnimatedBuilder(
+                animation: _rotationAnimation,
+                builder: (context, child) {
+                  return Transform(
+                    alignment: FractionalOffset.center,
+                    transform: Matrix4.rotationY(_rotationAnimation.value * 3.14),
+                    child: _rotationAnimation.value <= 0.5
+                        ? Text(widget.flashcard.word,
+                            style: const TextStyle(fontSize: 28))
+                        : Transform(
+                            alignment: Alignment.center,
+                            transform: Matrix4.rotationY(3.14),
+                            child: Text(widget.flashcard.translation,
+                                style: const TextStyle(fontSize: 28)),
+                          ),
+                  );
+                },
+              ),
+              AnimatedBuilder(
+                animation: _rotationAnimation,
+                builder: (context, child) {
+                  return SlideTransition(
+                    position: _slideDownAnimation,
+                    child: FadeTransition(
+                      opacity: _fadeInFadeOutAnimation,
+                      child: Visibility(
+                        visible: _rotationAnimation.value > 0.5,
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            const SizedBox(height: 20),
+                            Text(
+                              widget.flashcard.definition,
+                              style: const TextStyle(
+                                  fontSize: 20, fontStyle: FontStyle.italic),
+                              textAlign: TextAlign.center,
+                            ),
+                            const SizedBox(height: 10),
+                            Wrap(
+                              spacing: 8.0, // gap between adjacent chips
+                              runSpacing: 4.0, // gap between lines
+                              children:
+                                  widget.flashcard.synonyms.map((String synonym) {
+                                return GestureDetector(
+                                  onTap: () {
+                                    Flashcard? flashcard = widget.flashcards
+                                        .firstWhere(
+                                            (card) => card.word == synonym,
+                                            orElse: () => null as Flashcard);
+                                    if (flashcard != null) {
+                                      Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                          builder: (context) => FlashcardPage(
+                                            flashcard: flashcard,
+                                            flashcards: widget.flashcards,
+                                          ),
+                                        ),
+                                      );
+                                    }
+                                  },
+                                  child: Chip(
+                                    label: Text(synonym),
+                                  ),
+                                );
+                              }).toList(),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ],
           ),
-        ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text(widget.flashcard.hint),
+            ),
+          );
+        },
+        child: const Icon(Icons.lightbulb_outline),
       ),
     );
   }
